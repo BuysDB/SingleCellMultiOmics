6bc8892521f85f9813737531830d111cff22d890
BuysDB <git@buysdb.nl>
2019-05-20 10:23:42 +0200

    Typos

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ else:
         long_description = f.read()
 
 version = {}
-with open(os.path.join(_here, '\singlecelmultiomics', 'version.py')) as f:
+with open(os.path.join(_here, '\singlecellmultiomics', 'version.py')) as f:
     exec(f.read(), version)
 
 setup(
@@ -24,7 +24,7 @@ setup(
     author_email='b.barbanson@hubrecht.eu',
     url='https://github.com/BuysDB/SingleCellMultiOmics',
     license='MPL-2.0',
-    packages=['singlecelmultiomics'],
+    packages=['singlecellmultiomics'],
 
   install_requires=[
        'pysam==0.15.0'
