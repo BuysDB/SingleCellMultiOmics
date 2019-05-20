c269c93f1558c584b5c0eed2a81d3c80a157483d
BuysDB <git@buysdb.nl>
2019-05-20 13:14:46 +0200

    Fixes #2

--- singlecellmultiomics/modularDemultiplexer/demux.py
+++ singlecellmultiomics/modularDemultiplexer/demux.py
@@ -109,18 +109,34 @@ if len(args.fastqfiles)==1:
 		args.fastqfiles = fqFiles
 
 class FastqHandle:
-	def __init__(self, path, pairedEnd=False, single_cell=False ):
+	def __init__(self, path, pairedEnd=False, single_cell=False, maxHandles=500 ):
 		self.pe = pairedEnd
 		self.sc = single_cell
+		self.path = path
 		if not self.sc:
 			if pairedEnd:
 				self.handles = [ gzip.open(path+'R1.fastq.gz', 'wt'),   gzip.open(path+'R2.fastq.gz', 'wt') ]
 			else:
 				self.handles = [ gzip.open(path+'reads.fastq.gz', 'wt') ]
-	def write(self, records ):
-		for handle, record in zip(self.handles, records):
-			handle.write(record)
+		else:
+			from handlelimiter.handlelimiter import HandleLimiter
+			self.handles = HandleLimiter(compressionLevel=1, maxHandles=maxHandles)
 
+	def write(self, records ):
+		if self.sc:
+			for readIdx, record in zip(('R1','R2'), records):
+				# Obtain cell from record:
+				cell = f"{record.tags.get('BI','no_cell_id')}.{record.tags.get('MX','unk')}"
+				self.handles.write(f'{self.path}.{cell}.{readIdx}.fastq.gz',str(record), method=1)
+		else:
+			for handle, record in zip(self.handles, records):
+				handle.write(record)
+	def close(self):
+		if self.sc:
+			self.handles.close()
+		else:
+			for handle in self.handles:
+				handle.close()
 
 # Load barcodes
 barcodeParser = barcodeFileParser.BarcodeParser(hammingDistanceExpansion=args.hd, barcodeDirectory=args.barcodeDir)
@@ -182,7 +198,7 @@ for library in libraries:
 		targetDir = f'{args.o}/{library}'
 		if not os.path.exists(targetDir):
 			os.makedirs(targetDir)
-		handle = FastqHandle(f'{args.o}/{library}/demultiplexed' , True)
+		handle = FastqHandle(f'{args.o}/{library}/demultiplexed' , True, single_cell=args.scsepf)
 
 		rejectHandle = FastqHandle(f'{args.o}/{library}/rejects' , True)
 
