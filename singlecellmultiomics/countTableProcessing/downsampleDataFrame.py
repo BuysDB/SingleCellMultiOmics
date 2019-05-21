2a66cc1b8de96f0d906574a5797d90768c6072bb
BuysDB <git@buysdb.nl>
2019-05-21 11:04:05 +0200

    Added downsample function

---
+++ singlecellmultiomics/countTableProcessing/downsampleDataFrame.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+import glob
+import math
+import re
+import sys
+import multiprocessing
+
+def downsampleRow(args):
+    row, targetSum = args
+    currentCount = row.sum()
+    downsampledRow = row.copy()
+    while currentCount>targetSum and currentCount!=0:
+        possible = downsampledRow[ (downsampledRow>0) ]
+        desiredTossCount = int(currentCount- targetSum)
+        probabilities = [p/currentCount for p in possible]
+        for indexToLower in np.random.choice(
+                possible.index, max(0,desiredTossCount),
+                replace=True, p=probabilities):
+            if downsampledRow[indexToLower]>0:
+                downsampledRow[indexToLower] -= 1
+
+        currentCount = downsampledRow.sum()
+    return downsampledRow
+
+def downsampleDataFrame(df,downsample_to,min_feature_abundance=50):
+
+    pool = multiprocessing.Pool(8)
+    try:
+        df = df.loc[:,df.sum() > downsample_to]
+        df = df.loc[df.sum(1)>min_feature_abundance,:]
+
+        subset = df.transpose()
+        dfDownsampled = subset.copy()
+        for idx,drow in enumerate(pool.map(downsampleRow, [ (row, downsample_to) for i,row in subset.iterrows()])):
+            dfDownsampled.iloc[idx,:] = drow
+    except Exception as e:
+        print(e)
+    pool.close()
+    return dfDownsampled.transpose()
