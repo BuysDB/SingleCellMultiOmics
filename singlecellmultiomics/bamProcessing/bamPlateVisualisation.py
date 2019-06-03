0117236a3e40d5e77b27a2abd4e96aeaa8b437e9
buysdb <i@buysdb.nl>
2019-06-03 23:08:11 +0200

    Converted to PlateStatistic class, step to #17

--- singlecellmultiomics/bamProcessing/bamPlateVisualisation.py
+++ singlecellmultiomics/bamProcessing/bamPlateVisualisation.py
@@ -62,16 +62,6 @@ def human_readable( value, targetDigits=2,fp=0):
 
     return('%s%s' % (sVal,suffix))
 
-argparser = argparse.ArgumentParser(
- formatter_class=argparse.ArgumentDefaultsHelpFormatter,
- description='Visualize single cell statistics on a plate plot')
-argparser.add_argument('-o',  type=str, help="output plot folder path, every library will be visualised as a separate plate", default='./plots/')
-
-argparser.add_argument('alignmentfiles',  type=str, nargs='*')
-args = argparser.parse_args()
-
-if not os.path.exists(args.o):
-    os.makedirs(args.o)
 
 # Visualize the following:
 # PER LIBRARY / DEMUX method
@@ -95,78 +85,105 @@ for ci in range(1,385):
 
 
 
-rawFragmentCount = collections.defaultdict( collections.Counter ) # (library, mux) -> cell -> counts
-usableCount = collections.defaultdict( collections.Counter ) # (library, mux) -> cell -> counts
-moleculeCount = collections.defaultdict( collections.Counter ) # (library, mux) -> cell -> counts
+class PlateStatistic(object):
 
-skipReasons = collections.Counter()
-for alignmentFile in args.alignmentfiles:
-    with pysam.AlignmentFile(alignmentFile) as f:
-        for read in f:
-            rawFragmentCount[(read.get_tag('LY'), read.get_tag('MX'))][read.get_tag('SM')] += 1
+    def __init__(self, args):
+        self.args = args
 
-            if  read.get_tag('MX').startswith('CS2'):
-                if read.has_tag('XT') or read.has_tag('EX'):
-                    if read.is_read1: # We only count reads2
-                        continue
-                    usableCount[(read.get_tag('LY'), read.get_tag('MX'))][read.get_tag('SM')] += 1
+        self.rawFragmentCount = collections.defaultdict( collections.Counter ) # (library, mux) -> cell -> counts
+        self.usableCount = collections.defaultdict( collections.Counter ) # (library, mux) -> cell -> counts
+        self.moleculeCount = collections.defaultdict( collections.Counter ) # (library, mux) -> cell -> counts
+        self.skipReasons = collections.Counter()
+    def processRead(self,read):
+        self.rawFragmentCount[(read.get_tag('LY'), read.get_tag('MX'))][read.get_tag('SM')] += 1
 
-                    if  read.has_tag('RC') and read.get_tag('RC')==1:
-                        moleculeCount[(read.get_tag('LY'), read.get_tag('MX'))][read.get_tag('SM')] += 1
-            else:
+        if  read.get_tag('MX').startswith('CS2'):
+            if read.has_tag('XT') or read.has_tag('EX'):
+                if read.is_read1: # We only count reads2
+                    return
+                self.usableCount[(read.get_tag('LY'), read.get_tag('MX'))][read.get_tag('SM')] += 1
 
-                if read.has_tag('DS'):
-                    if not read.is_read1:
-                        skipReasons['Not R1'] += 1
-                        continue
-
-                    usableCount[(read.get_tag('LY'), read.get_tag('MX'))][read.get_tag('SM')] += 1
-                    if read.get_tag('RC')==1:
-                        moleculeCount[(read.get_tag('LY'), read.get_tag('MX'))][read.get_tag('SM')] += 1
-                else:
-                    skipReasons['No DS'] += 1
-
-
-# Visualize:
-for data, name in [(rawFragmentCount,'raw_reads'),(usableCount,'usable_reads'),(moleculeCount, 'unique_molecules')]:
-
-    for (library, mux), cellCounts in data.items():
-
-        df = pd.DataFrame( {name:cellCounts} )
-        if mux=='CS2C8U6':
-            offset = 0
+                if  read.has_tag('RC') and read.get_tag('RC')==1:
+                    self.moleculeCount[(read.get_tag('LY'), read.get_tag('MX'))][read.get_tag('SM')] += 1
         else:
-            offset = 1
-        df['col'] =  [ index2well[384][(offset+int(x.rsplit('_')[-1]))][1]  for x in df.index]
 
-        #df['row'] = [ index2well[384][(offset+int(x.rsplit('_')[-1]))][0]  for x in df.index]
-        df['row'] = [ -rows.index(index2well[384][(offset+int(x.rsplit('_')[-1]))][0])  for x in df.index]
-        df['size'] = (df[name]/np.percentile(df[name],99)*200)
-        df.plot.scatter(x='col',y='row',s=df['size'],
-                       c=[(0.2,0.2,0.5,0.9)]
-                       )
+            if read.has_tag('DS'):
+                if not read.is_read1:
+                    self.skipReasons['Not R1'] += 1
+                    return
 
-        # Annotate the outliers with values:
-        ax = plt.gca()
-        for ii,row in df.iterrows():
-            if row[name]>0 and (row[name]<np.percentile(df[name],5) or row[name]>np.percentile(df[name],95)):
-                text = ax.annotate( human_readable(int(row[name])), ( row['col'], row['row']),
-                ha='center',va='center_baseline',color='w',size=7)
-                text.set_path_effects([path_effects.Stroke(linewidth=3, foreground='black'),
-                       path_effects.Normal()])
+                self.usableCount[(read.get_tag('LY'), read.get_tag('MX'))][read.get_tag('SM')] += 1
+                if read.get_tag('RC')==1:
+                    self.moleculeCount[(read.get_tag('LY'), read.get_tag('MX'))][read.get_tag('SM')] += 1
+            else:
+                self.skipReasons['No DS'] += 1
 
-        plt.yticks(sorted(df['row'].unique())[::-1], sorted(rows) , rotation=0)
-        plt.xticks(sorted(df['col'].unique()), sorted(columns) , rotation=0)
-        plt.title(fr'{name} with ${mux}$ adapter'+f'\n{library}')
 
-        # Create legend:
-        #ld = []
-        #for x in np.linspace(1, max(df[name]), 4):
-    #        size = (x/np.percentile(df[name],99))*200
-        #    ld.append( mlines.Line2D([], [], color='blue', marker='.', linestyle='None',
-        ##                  markersize=np.sqrt(size), label=f'{int(x)}:{size}'))
-        #plt.legend(handles=ld)
+    def __repr__(self):
+        return 'Plate statistic'
 
-        plt.savefig(args.o + f'/{name}_{mux}_{library}.png')
+    def plot(self, target_path, title=None):
+        for data, name in [
+            (self.rawFragmentCount,'raw_reads'),
+            (self.usableCount,'usable_reads'),
+            (self.moleculeCount, 'unique_molecules')]:
 
-        plt.close()
+            for (library, mux), cellCounts in data.items():
+
+                df = pd.DataFrame( {name:cellCounts} )
+                if mux=='CS2C8U6':
+                    offset = 0
+                else:
+                    offset = 1
+                df['col'] =  [ index2well[384][(offset+int(x.rsplit('_')[-1]))][1]  for x in df.index]
+
+                #df['row'] = [ index2well[384][(offset+int(x.rsplit('_')[-1]))][0]  for x in df.index]
+                df['row'] = [ -rows.index(index2well[384][(offset+int(x.rsplit('_')[-1]))][0])  for x in df.index]
+                df['size'] = (df[name]/np.percentile(df[name],99)*200)
+                df.plot.scatter(x='col',y='row',s=df['size'],
+                               c=[(0.2,0.2,0.5,0.9)]
+                               )
+
+                # Annotate the outliers with values:
+                ax = plt.gca()
+                for ii,row in df.iterrows():
+                    if row[name]>0 and (row[name]<np.percentile(df[name],5) or row[name]>np.percentile(df[name],95)):
+                        text = ax.annotate( human_readable(int(row[name])), ( row['col'], row['row']),
+                        ha='center',va='center_baseline',color='w',size=7)
+                        text.set_path_effects([path_effects.Stroke(linewidth=3, foreground='black'),
+                               path_effects.Normal()])
+
+                plt.yticks(sorted(df['row'].unique())[::-1], sorted(rows) , rotation=0)
+                plt.xticks(sorted(df['col'].unique()), sorted(columns) , rotation=0)
+                plt.title(fr'{name} with ${mux}$ adapter'+f'\n{library}')
+
+                # Create legend:
+                #ld = []
+                #for x in np.linspace(1, max(df[name]), 4):
+            #        size = (x/np.percentile(df[name],99))*200
+                #    ld.append( mlines.Line2D([], [], color='blue', marker='.', linestyle='None',
+                ##                  markersize=np.sqrt(size), label=f'{int(x)}:{size}'))
+                #plt.legend(handles=ld)
+
+                plt.savefig(target_path.replace('.png','') + f'{name}_{mux}_{library}.png')
+                plt.close()
+
+if __name__=='__main__':
+    argparser = argparse.ArgumentParser(
+     formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+     description='Visualize single cell statistics on a plate plot')
+    argparser.add_argument('-o',  type=str, help="output plot folder path, every library will be visualised as a separate plate", default='./plots/')
+
+    argparser.add_argument('alignmentfiles',  type=str, nargs='*')
+    args = argparser.parse_args()
+
+    if not os.path.exists(args.o):
+        os.makedirs(args.o)
+
+    ps = PlateStatistic(args)
+    for alignmentFile in args.alignmentfiles:
+        with pysam.AlignmentFile(alignmentFile) as f:
+            for read in f:
+                ps.processRead(read)
+
+    ps.plot(args.o+'/PS')
