8e475513e6cb44309c0e0b3c385607f20645250f
BuysDB <git@buysdb.nl>
2019-06-21 13:44:09 +0200

    scchic demux is not ready for production yet.

--- singlecellmultiomics/modularDemultiplexer/demultiplexModules/scCHIC.py
+++ singlecellmultiomics/modularDemultiplexer/demultiplexModules/scCHIC.py
@@ -1,27 +1,16 @@
 from singlecellmultiomics.modularDemultiplexer.baseDemultiplexMethods import UmiBarcodeDemuxMethod
 
-# NLAIII, 96 well format with 3bp UMI
-class NLAIII_96w_c8_u3(UmiBarcodeDemuxMethod):
-	def __init__(self, barcodeFileParser,  **kwargs ):
-		self.barcodeFileAlias = 'lennart96NLA'
-		UmiBarcodeDemuxMethod.__init__(self,
-		umiRead=0, umiStart = 0, umiLength=3,
-		barcodeRead=0, barcodeStart = 3, barcodeLength=8,
-	 	barcodeFileAlias = self.barcodeFileAlias ,barcodeFileParser=barcodeFileParser,  **kwargs  )
-		self.shortName = 'NLAIII96C8U3'
-		self.longName = 'NLAIII, 96well CB: 8bp UMI: 3bp'
-		self.autoDetectable = True
-		self.description = '96 well format. 3bp umi followed by 8bp barcode'
-
-# NLAIII, 384 well format with 3bp UMI
-class NLAIII_384w_c8_u3(UmiBarcodeDemuxMethod):
+# SCCHIC using NLAIII adapter, 384 well format with 3bp UMI followed by "A" base
+"""
+class SCCHIC_384w_c8_u3(UmiBarcodeDemuxMethod):
 	def __init__(self, barcodeFileParser,  **kwargs ):
 		self.barcodeFileAlias = 'maya_384NLA'
 		UmiBarcodeDemuxMethod.__init__(self,
 		umiRead=0, umiStart = 0, umiLength=3,
 		barcodeRead=0, barcodeStart = 3, barcodeLength=8,
 	 	barcodeFileAlias = self.barcodeFileAlias ,barcodeFileParser=barcodeFileParser,  **kwargs  )
-		self.shortName = 'NLAIII384C8U3'
-		self.longName = 'NLAIII, 384well CB: 8bp UMI: 3bp'
+		self.shortName = 'SCHIC384C8U3'
+		self.longName = 'Single cell CHIC, 384well CB: 8bp UMI: 3bp'
 		self.autoDetectable = True
-		self.description = '384 well format. 3bp umi followed by 8bp barcode'
+		self.description = '384 well format. 3bp umi followed by 8bp barcode and a single A'
+"""
