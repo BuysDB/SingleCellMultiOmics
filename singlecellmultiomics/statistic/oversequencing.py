5a3e8976ccab6ffbaaad34413f2fcc01185a2657
BuysDB <git@buysdb.nl>
2019-06-27 20:08:51 +0200

    Added UMI oversequencing plot

--- singlecellmultiomics/statistic/oversequencing.py
+++ singlecellmultiomics/statistic/oversequencing.py
@@ -4,6 +4,12 @@ from .statistic import StatisticHistogram
 import singlecellmultiomics.pyutils as pyutils
 import collections
 
+import matplotlib
+matplotlib.rcParams['figure.dpi'] = 160
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+
 class OversequencingHistogram(StatisticHistogram):
     def __init__(self,args):
         StatisticHistogram.__init__(self, args)
@@ -16,3 +22,17 @@ class OversequencingHistogram(StatisticHistogram):
                 self.histogram[overseq-1]-=1
     def __repr__(self):
         return f'The average oversequencing is {pyutils.meanOfCounter(self.histogram)}, SD:{pyutils.varianceOfCounter(self.histogram)}'
+
+    def plot(self, target_path, title=None):
+        fig, ax = plt.subplots()
+        overseqRange = list(range(1,20))
+
+        ax.scatter( overseqRange, [self.histogram[x] for x in overseqRange])
+        if title is not None:
+            ax.set_title(title)
+        ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+        ax.set_xlabel("UMI Oversequenced")
+        ax.set_ylabel("# Fragments")
+        plt.tight_layout()
+        plt.savefig(target_path)
+        plt.close()
