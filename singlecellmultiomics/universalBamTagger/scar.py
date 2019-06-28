349f96052c661eb98c171dd05398efe0069e3391
BuysDB <git@buysdb.nl>
2019-06-28 16:59:05 +0200

    Added numpy import

--- singlecellmultiomics/universalBamTagger/scar.py
+++ singlecellmultiomics/universalBamTagger/scar.py
@@ -1,6 +1,7 @@
 from singlecellmultiomics.universalBamTagger.digest import DigestFlagger
 from singlecellmultiomics.tagtools import tagtools
 from singlecellmultiomics.utils.sequtils import phred_to_prob
+import numpy as np
 
 class ScarFlagger( DigestFlagger ):
 
