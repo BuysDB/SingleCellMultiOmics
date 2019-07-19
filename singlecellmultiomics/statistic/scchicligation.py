3c0c9e71dc67a93dc9cd3f63fe34bef7f2751890
BuysDB <git@buysdb.nl>
2019-07-19 18:53:10 +0200

    Make both A and TA plot.

--- singlecellmultiomics/statistic/scchicligation.py
+++ singlecellmultiomics/statistic/scchicligation.py
@@ -13,15 +13,19 @@ from matplotlib.ticker import MaxNLocator
 
 class ScCHICLigation():
     def __init__(self,args):
-        self.per_cell_ta_obs = collections.defaultdict( collections.Counter ) # cell -> { A_start: count, total_cuts: count }
+        self.per_cell_a_obs = collections.defaultdict( collections.Counter ) # cell -> { A_start: count, total_cuts: count }
+        self.per_cell_ta_obs = collections.defaultdict( collections.Counter ) # cell -> { TA_start: count, total_cuts: count }
 
     def processRead(self,read):
         if read.has_tag('RZ') and not read.is_duplicate and read.is_read1:
             sample = read.get_tag('SM')
-            #first = read.get_tag('RZ')[0]
+            first = read.get_tag('RZ')[0]
             if read.get_tag('RZ')=='TA':
                 self.per_cell_ta_obs[sample][ 'TA_start' ] += 1
+            if first=='A':
+                self.per_cell_a_obs[sample][ 'A_start' ] += 1
             self.per_cell_ta_obs[sample][ 'total' ] += 1
+            self.per_cell_a_obs[sample][ 'total' ] += 1
 
     def __repr__(self):
         return 'ScCHICLigation: no description'
@@ -30,6 +34,8 @@ class ScCHICLigation():
         return iter(self.per_cell_ta_obs)
 
     def plot(self, target_path, title=None):
+
+        ########### TA ###########
         fig, ax = plt.subplots(figsize=(4,4))
 
         x = []
@@ -49,8 +55,32 @@ class ScCHICLigation():
         ax.set_xlim(1,None)
         ax.set_ylim(-0.5,1.05)
         plt.tight_layout()
-        plt.savefig(target_path)
+        plt.savefig(target_path.replace('.png','.TA.png'))
         plt.close()
+
+        ########### A ###########
+        fig, ax = plt.subplots(figsize=(4,4))
+
+        x = []
+        y = []
+        for cell, cell_data in self.per_cell_ta_obs.items():
+            x.append(cell_data['total'] )
+            y.append( cell_data['A_start'] /  cell_data['total'] )
+
+
+        ax.scatter(x,y)
+        ax.set_xscale('log')
+        if title is not None:
+            ax.set_title(title)
+
+        ax.set_ylabel("Fraction unique cuts starting with A")
+        ax.set_xlabel("# Molecules")
+        ax.set_xlim(1,None)
+        ax.set_ylim(-0.5,1.05)
+        plt.tight_layout()
+        plt.savefig(target_path.replace('.png','.A.png'))
+        plt.close()
+
 
 
     def to_csv(self, path):
