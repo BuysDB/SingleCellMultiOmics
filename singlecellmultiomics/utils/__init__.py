58026ff397c6c60b5db43560e83dc60bae1aa676
BuysDB <git@buysdb.nl>
2019-07-23 10:56:55 +0200

    Added html utils module

--- singlecellmultiomics/utils/__init__.py
+++ singlecellmultiomics/utils/__init__.py
@@ -1,1 +1,2 @@
 from .sequtils import *
+from .html import *
---
+++ singlecellmultiomics/utils/html.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def style_str(s, color='black',weight=300):
+    """
+    Style the supplied string with HTML tags
+
+    Args:
+        s (str):
+            string to format
+
+        color( str ):
+            color to show the string in
+
+        weight( int ):
+            how thick the string will be displayed
+
+    Returns:
+        html(string) : html representation of the string
+
+    """
+
+    return f'<text style="color:{color}; font-weight:{weight}" >{s}</text>'
