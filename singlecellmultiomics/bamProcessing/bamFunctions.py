5d099de8aeae1246a7ef97e561e81240c46b2b5e
BuysDB <git@buysdb.nl>
2019-08-01 18:12:20 +0200

    Added ignore_missing flag

--- singlecellmultiomics/bamProcessing/bamFunctions.py
+++ singlecellmultiomics/bamProcessing/bamFunctions.py
@@ -1,11 +1,11 @@
 import os
-def get_reference_from_pysam_alignmentFile(pysam_AlignmentFile):
+def get_reference_from_pysam_alignmentFile(pysam_AlignmentFile, ignore_missing=False):
     try:
         for x in pysam_AlignmentFile.header.as_dict()['PG']:
             if  x.get('ID')!='bwa':
                 continue
             for argument in x.get('CL').split():
-                if (argument.endswith('.fa') or argument.endswith('.fasta') or argument.endswith('.fasta.gz') or argument.endswith('.fa.gz')) and os.path.exist(argument):
+                if (argument.endswith('.fa') or argument.endswith('.fasta') or argument.endswith('.fasta.gz') or argument.endswith('.fa.gz')) and (ignore_missing or os.path.exist(argument)):
                     return argument
     except Exception as e:
         pass
