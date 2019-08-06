dc31e0b427f34f1e4284715ab70d4c1aeb5a0777
BuysDB <git@buysdb.nl>
2019-08-06 13:27:00 +0200

    Added sample select argument. Added context selection argument. Added internal molecule identifier (an integer) to molecule Name.

--- singlecellmultiomics/universalBamTagger/tapsTabulator.py
+++ singlecellmultiomics/universalBamTagger/tapsTabulator.py
@@ -17,8 +17,15 @@ if __name__=='__main__':
     argparser.add_argument('-head',  type=int)
     argparser.add_argument('-contig',  type=str,help='contig to run on, all when not specified')
     argparser.add_argument('-moleculeNameSep',  type=str,help='Separator to use in molecule name', default=':')
+    argparser.add_argument('-samples',  type=str,help='Samples to select, separate with comma. For example CellA,CellC,CellZ', default=None)
+    argparser.add_argument('-context',  type=str,help='Contexts to select, separate with comma. For example Z,H,X', default=None)
     args = argparser.parse_args()
     alignments = pysam.AlignmentFile(args.alignmentfile)
+
+    samples = None if args.samples is None else set(args.samples.split(','))
+    contexts = None if args.context is None else set(
+        [x.upper() for x in  args.context.split(',')] +
+        [x.lower() for x in  args.context.split(',')])
 
     if args.ref is None:
         args.ref = bf.get_reference_from_pysam_alignmentFile(alignments)
@@ -42,9 +49,18 @@ if __name__=='__main__':
             if molecule.methylation_call_dict is None:
                 continue
 
+            # Skip sample if not selected
+            if samples is not None and molecule.sample not in samples:
+                continue
+
             for (chromosome, location),call in molecule.methylation_call_dict.items():
                 if call=='.': # Only print calls concerning C's
                     continue
-                print(f'{molecule.sample}{args.moleculeNameSep}{molecule.umi}{args.moleculeNameSep}{molecule.get_strand_repr()}\t{chromosome}\t{location}\t{call}')
+
+                # Skip non-selected contexts
+                if contexts is not None and call not in contexts:
+                    continue
+
+                print(f'{molecule.sample}{args.moleculeNameSep}{i}{args.moleculeNameSep}{molecule.umi}{args.moleculeNameSep}{molecule.get_strand_repr()}\t{chromosome}\t{location}\t{call}')
     except (KeyboardInterrupt,BrokenPipeError) as e:
         pass
