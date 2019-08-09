2edb5a4dc4b01df38c191ce29e0e6cfc59b6ffa1
BuysDB <git@buysdb.nl>
2019-08-09 14:44:13 +0200

    Added vanilla taps table generator

--- singlecellmultiomics/universalBamTagger/tapsTagger.py
+++ singlecellmultiomics/universalBamTagger/tapsTagger.py
@@ -12,28 +12,64 @@ import pysamiterators
 import sys
 import os
 import uuid
+import singlecellmultiomics.bamProcessing.bamFunctions as bf
+
+class Fraction:
+    def __init__(self):
+        self.values = [0,0]
+    def __setitem__(self, key, value):
+        self.values[key] = value
+    def __getitem__(self, key):
+        return self.values[key]
+
+    def __float__(self):
+        if sum(self.values)==0:
+            return np.nan
+        return self.values[1]/(self.values[1]+self.values[0])
+
 if __name__=='__main__':
+
+
     argparser = argparse.ArgumentParser(
      formatter_class=argparse.ArgumentDefaultsHelpFormatter,
      description='Add methylation information to BAM file')
 
     argparser.add_argument('alignmentfile',  type=str)
     argparser.add_argument('-o',  type=str, help="output BAM path", required=True)
+    argparser.add_argument('-table',  type=str, help="output table alias", required=False)
 
-    argparser.add_argument('-ref',  type=str, required=True, help='path to reference fasta file ')
+    argparser.add_argument('-bin_size',  type=int, default=250_000)
+    argparser.add_argument('-sliding_increment',  type=int, default=50_000)
 
+
+    argparser.add_argument('-ref',  type=str, required=False, help='path to reference fasta file ')
+    argparser.add_argument('-min_mq',  default=20, type=int, help='Min mapping qual')
+    argparser.add_argument('-uhd',  default=1, type=int, help='Umi hamming distance')
     argparser.add_argument('-head',  type=int)
     argparser.add_argument('-contig',  type=str,help='contig to run on')
+
+    argparser.add_argument('-samples',  type=str,help='Samples to select, separate with comma. For example CellA,CellC,CellZ', default=None)
+    argparser.add_argument('-context',  type=str,help='Contexts to select, separate with comma. For example Z,H,X', default=None)
     args = argparser.parse_args()
 
+    samples = None if args.samples is None else set(args.samples.split(','))
+    contexts = None if args.context is None else set(
+        [x.upper() for x in  args.context.split(',')] +
+        [x.lower() for x in  args.context.split(',')])
 
-    g= pysam.AlignmentFile(args.alignmentfile)
+    alignments= pysam.AlignmentFile(args.alignmentfile)
+    # Auto detect reference:
+    if args.ref is None:
+        args.ref = bf.get_reference_from_pysam_alignmentFile(alignments)
+        if args.ref is None:
+            raise ValueError("Supply reference, -ref")
+
     if args.contig is None:
         # Create jobs for all chromosomes:
         temp_prefix = os.path.abspath( os.path.dirname(args.o) )+ '/' + str(uuid.uuid4())
         hold_merge=[]
-        for chrom in g.references:
-            if chrom.startswith('chrUn') or chrom.endswith('_random'):
+        for chrom in alignments.references:
+            if chrom.startswith('chrUn') or chrom.endswith('_random') or 'ERCC' in chrom:
                 continue
             temp_bam_path = f'{temp_prefix}_{chrom}.bam'
             arguments = " ".join([x for x in sys.argv if not x==args.o in x and x!='-o'])  + f" -contig {chrom} -o {temp_bam_path}"
@@ -49,71 +85,64 @@ if __name__=='__main__':
     taps = singlecellmultiomics.molecule.TAPS(reference=reference)
     temp_out = f'{args.o}.temp.out.bam'
 
-    with pysam.AlignmentFile(temp_out , "wb",header=g.header) as output:
-        for i,molecule in  enumerate( singlecellmultiomics.molecule.MoleculeIterator(
-            alignments=g,
-            moleculeClass=singlecellmultiomics.molecule.Molecule,
-            fragmentClass=singlecellmultiomics.fragment.NLAIIIFragment,
-            fragment_class_args={'umi_hamming_distance':1},
-            contig=args.contig)):
-            if args.head and i>args.head:
-                break
-            # Obtain taps methylation calls:
+    # Methylation dictionary: site->cell->value
+    binned_data = collections.defaultdict(lambda: collections.defaultdict(Fraction))
+    cell_count=collections.Counter()
 
-            if molecule.is_multimapped():
+    with pysam.AlignmentFile(temp_out , "wb",header=alignments.header) as output:
+
+        for i,molecule in  enumerate(
+            singlecellmultiomics.molecule.MoleculeIterator(
+                    alignments=alignments,
+                    moleculeClass=singlecellmultiomics.molecule.TAPSNlaIIIMolecule,
+                    fragmentClass=singlecellmultiomics.fragment.NLAIIIFragment,
+                    fragment_class_args={'umi_hamming_distance':args.uhd},
+                    yield_invalid=True,
+                    molecule_class_args={
+                       # 'features':transcriptome_features,
+                        'reference':reference,
+                        'site_has_to_be_mapped':True,
+                        #radius in order to capture spliced transcripts
+                        'taps':taps,
+                        'min_max_mapping_quality':args.min_mq
+                    }
+            )):
+
+            if not molecule.is_valid():
+                molecule.set_meta('RF','rejected_molecule')
+                molecule.write_tags()
+                molecule.write_pysam(output)
                 continue
 
-            # Find all aligned positions and corresponding reference bases:
-            aligned_reference_positions = {} #(chrom,pos)->base
-            for read in molecule.iter_reads():
-                for read_pos, ref_pos, ref_base in read.get_aligned_pairs(with_seq=True, matches_only=True):
-                    aligned_reference_positions[(read.reference_name,ref_pos)] = ref_base.upper()
 
-            # Obtain consensus:
-            try:
-                consensus = molecule.get_consensus()
-            except ValueError:
-                continue # we cannot obtain a consensus
+            if args.table is not None:
+                for (chromosome, location),call in molecule.methylation_call_dict.items():
+                    if call=='.': # Only use calls concerning C's
+                        continue
+                    # Skip non-selected contexts
+                    if contexts is not None and call not in contexts:
+                        continue
+                    for binIdx in singlecellmultiomics.utils.coordinate_to_bins(location, args.bin_size, args.sliding_increment):
+                        binned_data[(chromosome, molecule.get_strand_repr(), binIdx)][molecule.get_sample()][call.isupper()]+=1
+                        cell_count[molecule.get_sample()]+=1
 
+            molecule.write_tags()
+            molecule.set_meta('RF','accepted_molecule')
+            molecule.write_pysam(output)
 
-            # if insecure about the consensus emit the reference, if no CG AT conversion skip
-            for location, reference_base in aligned_reference_positions.items():
-                if not location in consensus or reference_base not in 'CG' or consensus[location] not in 'AT':
-                    consensus[location] = reference_base.upper()
-
-            # find all locations where a C/G was converted into A/T, now strand specific
-            converted_bases = 0
-            conversions = {}
-            for location, reference_base in aligned_reference_positions.items():
-                if (not molecule.strand and reference_base=='C' and consensus[location] in 'CT') or \
-                    molecule.strand and reference_base=='G' and consensus[location] in 'AG':
-                    conversions[location] = {'ref':reference_base, 'obs':consensus[location]}
-                    if consensus[location] in 'TA':
-                        converted_bases+=1
-
-            # obtain the context of the conversions:
-            conversion_contexts  ={
-                location : taps.position_to_context(
-                    *location,
-                    observed_base = observations['obs'],
-                    strand = molecule.strand)[1]
-                for location, observations in conversions.items()}
-
-            # Write bismark tags:
-            molecule.set_methylation_call_tags(conversion_contexts)
-
-            for read in molecule.iter_reads():
-                if molecule.strand == 0: # forward
-                    read.set_tag('XR','GA')
-                    read.set_tag('XG','GA')
-                else:
-                    read.set_tag('XR','GA')
-                    read.set_tag('XG','CT')
-            
-            # Write all reads to the new bam file:
-            for read in molecule.iter_reads():
-                if read is not None:
-                    output.write(read)
+    if args.table is not None:
+        #cast all fractions to float
+        for loc in binned_data:
+            for sample in binned_data[loc]:
+                binned_data[loc][sample] = float(binned_data[loc][sample])
+        df = pd.DataFrame(binned_data)
+        del binned_data
+        if args.contig:
+            df.to_pickle(f'{args.table}_{args.contig}.pickle.gz')
+            df.to_csv(f'{args.table}_{args.contig}.csv')
+        else:
+            df.to_pickle(f'{args.table}.pickle.gz')
+            df.to_csv(f'{args.table}.csv')
 
     # Sort and index
     # Perform a reheading, sort and index
