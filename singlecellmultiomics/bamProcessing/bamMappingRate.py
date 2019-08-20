50fb737281042190a18f5e4b3d2cfcf5dc832790
BuysDB <git@buysdb.nl>
2019-08-20 10:57:33 +0200

    Dont crash on empty file

--- singlecellmultiomics/bamProcessing/bamMappingRate.py
+++ singlecellmultiomics/bamProcessing/bamMappingRate.py
@@ -20,21 +20,23 @@ for library in args.libraries:
     unpaired_reads_total = 0
     total_reads = 0
 
+    try:
+        os.system(f'samtools idxstats ./{library}/sorted.bam > ./{library}/stats.tsv')
+        with open(f'./{library}/stats.tsv') as f:
+            for line in f:
+                chrom, chlen, mapped_reads, unmapped_reads = line.strip().split()
+                mapped_reads=int(mapped_reads)
+                unmapped_reads=int(unmapped_reads)
 
-    os.system(f'samtools idxstats ./{library}/sorted.bam > ./{library}/stats.tsv')
-    with open(f'./{library}/stats.tsv') as f:
-        for line in f:
-            chrom, chlen, mapped_reads, unmapped_reads = line.strip().split()
-            mapped_reads=int(mapped_reads)
-            unmapped_reads=int(unmapped_reads)
+                total_reads+=unmapped_reads
+                total_reads+=mapped_reads
 
-            total_reads+=unmapped_reads
-            total_reads+=mapped_reads
+                if chrom=='*':
+                    unmapped_reads_total+=unmapped_reads
 
-            if chrom=='*':
-                unmapped_reads_total+=unmapped_reads
-
-            else:
-                mapped_reads_total += mapped_reads
-                unpaired_reads_total+=unmapped_reads
-    print(f'{library}\t{colorama.Fore.GREEN}{mapped_reads_total}\t{colorama.Fore.RED}{unmapped_reads_total+unpaired_reads_total}\t{colorama.Style.RESET_ALL}{mapped_reads_total/total_reads}')
+                else:
+                    mapped_reads_total += mapped_reads
+                    unpaired_reads_total+=unmapped_reads
+        print(f'{library}\t{colorama.Fore.GREEN}{mapped_reads_total}\t{colorama.Fore.RED}{unmapped_reads_total+unpaired_reads_total}\t{colorama.Style.RESET_ALL}{mapped_reads_total/total_reads}')
+    except Exception as e:
+        continue
