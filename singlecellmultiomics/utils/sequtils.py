c10f90f2fdaaa2fed49f5ce31675e71f08ed2b5b
BuysDB <git@buysdb.nl>
2019-08-27 16:08:40 +0200

    Added reverse complement and complement methods

--- singlecellmultiomics/utils/sequtils.py
+++ singlecellmultiomics/utils/sequtils.py
@@ -1,6 +1,12 @@
 import math
 
 def phred_to_prob(phred):
+    """Convert a phred score (ASCII) to a numeric probability
+
+    returns:
+        probability(float)
+    """
+
     try:
         return math.pow(10,-(ord(phred)-33)/10 )
     except ValueError:
@@ -8,3 +14,20 @@ def phred_to_prob(phred):
 
 def hamming_distance(a,b):
     return sum((i!=j and i!='N' and j!='N' for i,j in zip(a,b)))
+
+complement_translate = str.maketrans('ATCGNatcgn','TAGCNtagcn')
+def reverse_complement(seq):
+    """Obtain reverse complement of seq
+
+    returns:
+        reverse complement (str)
+    """
+    return seq.translate(complement_translate)[::-1]
+
+def complement(seq):
+    """Obtain complement of seq
+
+    returns:
+        complement (str)
+    """
+    return seq.translate(complement_translate)
