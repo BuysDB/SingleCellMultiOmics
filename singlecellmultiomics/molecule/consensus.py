5ea247d5ab53ce0d3818d8091e9b2fd324471098
BuysDB <git@buysdb.nl>
2019-10-20 11:38:35 +0200

    Report out-of-bag score and feature matrix dimensions

--- singlecellmultiomics/molecule/consensus.py
+++ singlecellmultiomics/molecule/consensus.py
@@ -2,14 +2,14 @@ import sklearn.ensemble
 import numpy as np
 
 def get_consensus_training_data(molecule_iterator, mask_variants=None, n_train=100_000):
-    X = []
+    X = None
     y = []
 
     for i,molecule in enumerate( molecule_iterator ):
         x,_y = molecule.get_base_calling_training_data(mask_variants)
         if X is None:
             X = np.empty((0,x.shape[1]))
-            print(f"Creating feature matrix with {x.shape[1]} dimensions")
+            print(f"Creating feature matrix with {x.shape[1]} dimensions and {n_train} training base-calls")
         y+=_y
         X = np.append(X,x,axis=0)
         if len(X)>=n_train:
@@ -25,4 +25,6 @@ def train_consensus_model(molecule_iterator, mask_variants=None, classifier=None
                 )
     X,y = get_consensus_training_data(molecule_iterator, mask_variants=mask_variants, n_train=n_train)
     classifier.fit(X,y)
+    if type(classifier)==sklearn.ensemble.forest.RandomForestClassifier:
+        print(f"Model out of bag accuracy: {classifier.oob_score_}")
     return classifier
