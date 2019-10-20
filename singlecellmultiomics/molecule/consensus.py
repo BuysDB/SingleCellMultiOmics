eaeb059c249a373a8652726b2af6721505949ef2
BuysDB <git@buysdb.nl>
2019-10-20 11:09:59 +0200

    Created functions to generate training datasets get_consensus_training_data and train_consensus_model

---
+++ singlecellmultiomics/molecule/consensus.py
@@ -0,0 +1,28 @@
+import sklearn.ensemble
+import numpy as np
+
+def get_consensus_training_data(molecule_iterator, mask_variants=None, n_train=100_000):
+    X = []
+    y = []
+
+    for i,molecule in enumerate( molecule_iterator ):
+        x,_y = molecule.get_base_calling_training_data(mask_variants)
+        if X is None:
+            X = np.empty((0,x.shape[1]))
+            print(f"Creating feature matrix with {x.shape[1]} dimensions")
+        y+=_y
+        X = np.append(X,x,axis=0)
+        if len(X)>=n_train:
+            break
+    return X,y
+
+def train_consensus_model(molecule_iterator, mask_variants=None, classifier=None, n_train=100_000):
+    if classifier is None: # default to random forest
+        classifier = sklearn.ensemble.RandomForestClassifier(
+                n_jobs=8,
+                n_estimators=100,
+                oob_score=True
+                )
+    X,y = get_consensus_training_data(molecule_iterator, mask_variants=mask_variants, n_train=n_train)
+    classifier.fit(X,y)
+    return classifier
