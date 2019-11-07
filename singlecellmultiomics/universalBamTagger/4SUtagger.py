df638a8645706e6cbc8afbeb464af08e8b69604f
BuysDB <git@buysdb.nl>
2019-11-07 16:27:44 +0100

    Cleaned up code, removed default parameters and fixed auto ref detection

--- singlecellmultiomics/universalBamTagger/4SUtagger.py
+++ singlecellmultiomics/universalBamTagger/4SUtagger.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import pysam
-from singlecellmultiomics.molecule import FourThiouridine
+from singlecellmultiomics.molecule import FourThiouridine, MoleculeIterator
 from singlecellmultiomics.fragment import FeatureCountsSingleEndFragment
 
 import singlecellmultiomics.pyutils
@@ -10,6 +10,7 @@ import glob
 import pickle
 import pandas as pd
 import argparse
+
 from singlecellmultiomics.bamProcessing.bamFunctions import sorted_bam_file, sort_and_index, get_reference_from_pysam_alignmentFile, add_readgroups_to_header, write_program_tag
 
 argparser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter, description="""
@@ -20,31 +21,26 @@ argparser.add_argument('-head', type=int)
 argparser.add_argument('-ref', type=str, help='reference (autodetected if not supplied)')
 args=  argparser.parse_args()
 
-reference = None
-if args.ref is None:
-    args.ref = get_reference_from_pysam_alignmentFile(input_bam)
 
-if args.ref is not None:
-    reference = pysamiterators.iterators.CachedFasta( pysam.FastaFile(args.ref) )
+with pysam.AlignmentFile(args.bamfile, "rb") as alignments:
 
+    reference = None
+    if args.ref is None:
+        args.ref = get_reference_from_pysam_alignmentFile(alignments)
 
-fragment_class_args = { 'umi_hamming_distance':0 }
-molecule_class_args = { 'reference':reference}
+    if args.ref is not None:
+        reference = pysamiterators.iterators.CachedFasta( pysam.FastaFile(args.ref) )
 
+    fragment_class_args = { 'umi_hamming_distance':0 }
+    molecule_class_args = { 'reference':reference}
+    converted_base_frequencies = collections.defaultdict( lambda: collections.defaultdict( collections.Counter ))
 
-converted_base_frequencies = collections.defaultdict( lambda: collections.defaultdict( collections.Counter ))
-
-with pysam.AlignmentFile(args.bamfile) as alignments:
-
-    with singlecellmultiomics.bamProcessing.sorted_bam_file(f'{args.bamfile}.4SU_tagged.bam',origin_bam=alignments) as out:
-        test_iter = singlecellmultiomics.molecule.MoleculeIterator(alignments,
+    with sorted_bam_file(f'{args.bamfile}.4SU_tagged.bam',origin_bam=alignments) as out:
+        test_iter =MoleculeIterator(alignments,
                 fragment_class_args=fragment_class_args,
                 molecule_class_args={ 'reference':reference},
-               moleculeClass=FourThiouridine,check_eject_every=1000,
-                pooling_method=0,
+               moleculeClass=FourThiouridine,
                fragmentClass=FeatureCountsSingleEndFragment
-                                                                   ,yield_invalid=False
-
             )
         for i,molecule in enumerate( test_iter ):
             molecule.write_tags()
@@ -54,7 +50,5 @@ with pysam.AlignmentFile(args.bamfile) as alignments:
             if args.head is not None and i>(args.head-1):
                 break
 
-
-
-for sample, data in converted_base_frequencies.items():
-    pd.DataFrame( data ).to_csv(f'{args.bamfile}.{sample}.counts.csv')
+    for sample, data in converted_base_frequencies.items():
+        pd.DataFrame( data ).to_csv(f'{args.bamfile}.{sample}.counts.csv')
