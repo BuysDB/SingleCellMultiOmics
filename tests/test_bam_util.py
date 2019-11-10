a1f421cd12369ea5f844bb488f0ab88a15591d13
BuysDB <git@buysdb.nl>
2019-11-10 22:53:00 +0100

    test case for the sorted bam file with read groups

--- tests/test_bam_util.py
+++ tests/test_bam_util.py
@@ -35,5 +35,29 @@ class TestSorted(unittest.TestCase):
         except Exception as e:
             pass
 
+    def test_write_to_read_grouped_sorted(self):
+        write_path = './data/write_test_rg.bam'
+        read_groups = set()
+        with pysam.AlignmentFile('./data/mini_nla_test.bam') as f:
+            with sorted_bam_file(write_path, origin_bam=f,read_groups=read_groups) as out:
+                for molecule in singlecellmultiomics.molecule.MoleculeIterator(
+                    alignments=f,
+                    moleculeClass=singlecellmultiomics.molecule.NlaIIIMolecule,
+                    fragmentClass=singlecellmultiomics.fragment.NLAIIIFragment,
+                    fragment_class_args={'umi_hamming_distance':0},
+                    pooling_method=0,
+                    yield_invalid=True
+                ):
+                    molecule.write_pysam(out)
+                    for frag in molecule:
+                        read_groups.add( frag.get_read_group() )
+
+        self.assertTrue(os.path.exists(write_path))
+        try:
+            os.remove(write_path)
+        except Exception as e:
+            pass
+
+
 if __name__ == '__main__':
     unittest.main()
