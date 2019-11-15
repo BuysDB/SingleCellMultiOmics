014be858ad735ceb585e839686effbab73d795b8
BuysDB <git@buysdb.nl>
2019-11-15 17:21:40 +0100

    Added tests which check if the program tag is written

--- tests/test_bam_util.py
+++ tests/test_bam_util.py
@@ -5,8 +5,9 @@ import singlecellmultiomics.molecule
 import singlecellmultiomics.fragment
 import pysam
 import pysamiterators.iterators
-from singlecellmultiomics.bamProcessing import sorted_bam_file
+from singlecellmultiomics.bamProcessing import sorted_bam_file,write_program_tag
 import os
+import sys
 """
 These tests check if the Molecule module is working correctly
 """
@@ -39,7 +40,23 @@ class TestSorted(unittest.TestCase):
         write_path = './data/write_test_rg.bam'
         read_groups = set()
         with pysam.AlignmentFile('./data/mini_nla_test.bam') as f:
-            with sorted_bam_file(write_path, origin_bam=f,read_groups=read_groups) as out:
+
+            input_header = f.header.as_dict()
+            write_program_tag(input_header,
+                program_name='test_bam_util_test1',
+                command_line = " ".join(sys.argv),
+                version = singlecellmultiomics.__version__,
+                description = f'a description'
+                )
+
+            write_program_tag(input_header,
+                program_name='test_bam_util_test2',
+                command_line = " ".join(sys.argv),
+                version = singlecellmultiomics.__version__,
+                description = f'a description'
+                )
+            #print([x for x in input_header['PG'] if not 'bwa mem' in x.get('CL','')])
+            with sorted_bam_file(write_path, header=input_header,read_groups=read_groups) as out:
                 for molecule in singlecellmultiomics.molecule.MoleculeIterator(
                     alignments=f,
                     moleculeClass=singlecellmultiomics.molecule.NlaIIIMolecule,
@@ -53,8 +70,15 @@ class TestSorted(unittest.TestCase):
                         read_groups.add( frag.get_read_group() )
 
         self.assertTrue(os.path.exists(write_path))
+
+        # Now test if the program tag is there...
+        with pysam.AlignmentFile(write_path) as f:
+            self.assertTrue( 1==len([x for x in f.header['PG'] if 'test_bam_util_test1' in x.get('PN','')]) )
+            self.assertTrue( 1==len([x for x in f.header['PG'] if 'test_bam_util_test2' in x.get('PN','')]) )
+
         try:
-            os.remove(write_path)
+            #os.remove(write_path)
+            pass
         except Exception as e:
             pass
 
