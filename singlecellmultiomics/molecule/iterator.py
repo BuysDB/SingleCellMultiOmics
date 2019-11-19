0a4c9c97800c685f35a95d106819aeea87d0658c
BuysDB <git@buysdb.nl>
2019-11-19 19:46:54 +0100

    Added more documentation on MoleculeIterator and added option to obtain molecules from list containing single end reads

--- singlecellmultiomics/molecule/iterator.py
+++ singlecellmultiomics/molecule/iterator.py
@@ -46,6 +46,109 @@ class MoleculeIterator():
 
         Yields:
             molecule (Molecule): Molecule
+
+
+        Example:
+            >>> !wget https://github.com/BuysDB/SingleCellMultiOmics/blob/master/data/mini_nla_test.bam?raw=true -O mini_nla_test.bam
+            >>> !wget https://github.com/BuysDB/SingleCellMultiOmics/blob/master/data/mini_nla_test.bam.bai?raw=true -O mini_nla_test.bam.bai
+            >>> import pysam
+            >>> from singlecellmultiomics.molecule import NlaIIIMolecule, MoleculeIterator
+            >>> from singlecellmultiomics.fragment import NLAIIIFragment
+            >>> import pysamiterators
+            >>> alignments = pysam.AlignmentFile('mini_nla_test.bam')
+            >>> for molecule in MoleculeIterator(
+            >>>             alignments=alignments,
+            >>>             moleculeClass=singlecellmultiomics.molecule.NlaIIIMolecule,
+            >>>             fragmentClass=singlecellmultiomics.fragment.NLAIIIFragment,
+            >>>         ):
+            >>>     break
+            >>> molecule
+            NlaIIIMolecule
+            with 1 assinged fragments
+            Allele :No allele assigned
+                Fragment:
+                sample:APKS1P25-NLAP2L2_57
+                umi:CCG
+                span:chr1 164834728-164834868
+                strand:+
+                has R1: yes
+                has R2: no
+                randomer trimmed: no
+                DS:164834865
+            RS:0
+            RZ:CAT
+            Restriction site:('chr1', 164834865)
+
+        Example:
+            Using an iterator instead of a SAM/BAM file
+            >>> from singlecellmultiomics.molecule import MoleculeIterator
+            >>> from singlecellmultiomics.fragment import Fragment
+            >>> import pysam
+            >>> # Create SAM file to write some example reads to:
+            >>> test_sam = pysam.AlignmentFile('test.sam','w',reference_names=['chr1','chr2'],reference_lengths=[1000,1000])
+            >>> read_A = pysam.AlignedSegment(test_sam.header)
+            >>> read_A.set_tag('SM','CELL_1')
+            >>> read_A.set_tag('RX','CAT')
+            >>> read_A.reference_name = 'chr1'
+            >>> read_A.reference_start = 100
+            >>> read_A.query_sequence = 'ATCGGG'
+            >>> read_A.cigarstring = '6M'
+            >>> read_A.mapping_quality = 60
+            >>> # Create a second read which is a duplicate of the previous
+            >>> read_B = pysam.AlignedSegment(test_sam.header)
+            >>> read_B.set_tag('SM','CELL_1')
+            >>> read_B.set_tag('RX','CAT')
+            >>> read_B.reference_name = 'chr1'
+            >>> read_B.reference_start = 100
+            >>> read_B.query_sequence = 'ATCGG'
+            >>> read_B.cigarstring = '5M'
+            >>> read_B.mapping_quality = 60
+            >>> # Create a thids read which is belonging to another cell
+            >>> read_C = pysam.AlignedSegment(test_sam.header)
+            >>> read_C.set_tag('SM','CELL_2')
+            >>> read_C.set_tag('RX','CAT')
+            >>> read_C.reference_name = 'chr1'
+            >>> read_C.reference_start = 100
+            >>> read_C.query_sequence = 'ATCGG'
+            >>> read_C.cigarstring = '5M'
+            >>> read_C.mapping_quality = 60
+            >>> # Set up an iterable containing the reads:
+            >>> reads = [  read_A,read_B,read_C ]
+            >>> molecules = []
+            >>> for molecule in MoleculeIterator( reads ):
+            >>>     print(molecule)
+            Molecule
+                with 2 assinged fragments
+                Allele :No allele assigned
+                    Fragment:
+                    sample:CELL_1
+                    umi:CAT
+                    span:chr1 100-106
+                    strand:+
+                    has R1: yes
+                    has R2: no
+                    randomer trimmed: no
+
+        	    Fragment:
+                    sample:CELL_1
+                    umi:CAT
+                    span:chr1 100-105
+                    strand:+
+                    has R1: yes
+                    has R2: no
+                    randomer trimmed: no
+
+            Molecule
+                    with 1 assinged fragments
+                    Allele :No allele assigned
+                        Fragment:
+                        sample:CELL_2
+                        umi:CAT
+                        span:chr1 100-105
+                        strand:+
+                        has R1: yes
+                        has R2: no
+                        randomer trimmed: no
         """
         if queryNameFlagger is None:
             queryNameFlagger = singlecellmultiomics.universalBamTagger.QueryNameFlagger()
@@ -110,7 +213,18 @@ class MoleculeIterator():
             # If an iterable is provided use this as read source:
             self.matePairIterator = self.alignments
 
-        for R1,R2 in self.matePairIterator:
+        for reads in self.matePairIterator:
+
+            if type(reads) is pysam.AlignedSegment:
+                R1 = reads
+                R2 = None
+            elif len(reads)==2:
+                R1,R2 = reads
+            elif (type(reads) is list or type(reads) is tuple) and len(reads)==1:
+                R1 = reads[0]
+                R2 = None
+            else:
+                raise ValueError('Iterable not understood, supply either  pysam.AlignedSegment or lists of  pysam.AlignedSegment')
             # Make sure the sample/umi etc tags are placed:
             if self.perform_qflag:
                 qf.digest([R1,R2])
