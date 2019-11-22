c9e57d23c314e3f6251d553723a4b80249ea981f
BuysDB <git@buysdb.nl>
2019-11-22 11:53:11 +0100

    Added version requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+o#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 from setuptools import setup
@@ -97,7 +97,7 @@ setup(
         ],
 
   install_requires=[
-       'pysam','numpy','pandas','colorama','pysamiterators','more-itertools','matplotlib','tabulate','wheel','setuptools','scikit-learn','seaborn','biopython'
+       'pysam>=0.15.3','numpy>=1.16.4','pandas>=0.25.0','colorama','pysamiterators>=1.6','more-itertools','matplotlib','tabulate','wheel','setuptools>=40.8.0','scikit-learn>=0.21.3','seaborn','biopython>=1.71','pytest>=5.0.0'
    ],
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
