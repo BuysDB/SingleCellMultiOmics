dd17db3411e9ee82c5bee717824ad109415c99fd
BuysDB <git@buysdb.nl>
2019-11-23 20:08:19 +0100

    Changed import from star to MoleculeIterator

--- singlecellmultiomics/molecule/__init__.py
+++ singlecellmultiomics/molecule/__init__.py
@@ -4,6 +4,6 @@ from .chic import *
 from .featureannotatedmolecule import *
 from .nlaIII import *
 from .rna import *
-from .iterator import *
+from .iterator import MoleculeIterator
 from .consensus import *
 from .fourthiouridine import *
