f1a918e06745bdb4c99e1a88f4dd9c64b382ef40
BuysDB <git@buysdb.nl>
2019-11-25 21:17:21 +0100

    Added --clean flag and shows message containing snakemake command

--- singlecellmultiomics/snakemake_workflows/scmo_workflow.py
+++ singlecellmultiomics/snakemake_workflows/scmo_workflow.py
@@ -12,7 +12,7 @@ def get_workflow_list():
     return [ workflow for workflow  in pkg_resources.resource_listdir('singlecellmultiomics','snakemake_workflows') if not workflow.endswith('.py')]
 
 
-def deploy_workflow_files(name, directory='./'):
+def deploy_workflow_files(name, clean=False, directory='./'):
     """ Deploy workflow configuration
 
     Args:
@@ -26,13 +26,23 @@ def deploy_workflow_files(name, directory='./'):
 
     base_path = f'snakemake_workflows/{name}'
     for file_to_copy in pkg_resources.resource_listdir('singlecellmultiomics',base_path):
-        copyfile(base_path, directory)
+        print(Fore.GREEN + f'Creating {file_to_copy}' + Style.RESET_ALL, end="\t")
+
+        target = directory+f'/{file_to_copy}'
+        if os.path.exists(target) and not clean:
+            print(Fore.RED + f'The file {target} already exists! Skipping!' + Style.RESET_ALL)
+        else:
+            copyfile( pkg_resources.resource_filename('singlecellmultiomics', base_path+'/'+file_to_copy),target )
+            print(Fore.GREEN + f'[ok]' + Style.RESET_ALL)
+
+    print(f"\nEdit config.json and run:\n{Fore.BLUE}snakemake -np --restart-times 3{Style.RESET_ALL}")
 
 if __name__=='__main__':
     argparser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         description='Obtain single cell multiomics workflow configuration files, deploys configuration in current directory')
     argparser.add_argument('alias', type=str, help= 'nla, chic', nargs='?')
+    argparser.add_argument('--clean', action='store_true',help='clear all existing files')
     args = argparser.parse_args()
 
     if args.alias is None:
@@ -41,4 +51,4 @@ if __name__=='__main__':
         print('\n'.join(get_workflow_list()))
 
     else:
-        deploy_workflow_files(args.alias)
+        deploy_workflow_files(args.alias, clean=args.clean)
