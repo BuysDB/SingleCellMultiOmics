64eb816e13db9a048ae6c5532d88e960ca7affd3
BuysDB <git@buysdb.nl>
2020-04-25 21:41:13 +0200

    Added more tests

--- tests/test_blockzip.py
+++ tests/test_blockzip.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import unittest
 from singlecellmultiomics.utils import BlockZip
+
 import os
 """
 These tests check if the BlockZip module is working correctly
@@ -24,12 +25,29 @@ class Test_BlockZip(unittest.TestCase):
             self.assertEqual( f[ ('chr2',2101,True) ], 'yes' )
             self.assertEqual( f[ ('chr1',0,True) ], None)
 
+
+        # Test verification:
+        with BlockZip(zip_path,'r') as f:
+            f.verify()
+
+        with BlockZip(zip_path,'w') as f:
+            f.write('chr1',101,False,'yes')
+            f.write('chr1',100,False,'yes')
+            f.write('chr2',2101,True,'yes')
+            f.write('chrX',0,False,'X')
+
+        with BlockZip(zip_path,'r') as bz:
+            self.assertRaises(ValueError, bz.verify)
+
+
         # Test for missing index:
         os.remove(zip_path+'.idx')
         self.assertRaises(ValueError, BlockZip, './non_existing.bgzf','r')
 
         os.remove(zip_path)
         self.assertRaises(ValueError, BlockZip, './non_existing.bgzf','r')
+
+
 
 
 if __name__ == '__main__':
