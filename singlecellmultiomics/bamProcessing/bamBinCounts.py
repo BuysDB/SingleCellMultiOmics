2b6972ddb65ea79292959a4c4e78fdc817f48d06
BuysDB <git@buysdb.nl>
2020-05-11 20:25:28 +0200

    Refactoring

--- singlecellmultiomics/bamProcessing/bamBinCounts.py
+++ singlecellmultiomics/bamProcessing/bamBinCounts.py
@@ -14,7 +14,7 @@ from datetime import datetime
 from itertools import chain
 from more_itertools import windowed
 from typing import Generator
-
+from singlecellmultiomics.methylation import MethylationCountMatrix
 
 def fill_range(start, end, step):
     """
@@ -446,7 +446,7 @@ def count_methylation_binned(args):
 
     min_counts_per_bin = kwargs.get('min_counts_per_bin',10) # Min measurements across all cells
     # Cant use defaultdict because of pickles :\
-    met_counts = {}  # Sample->(contig,bin_start,bin_end)-> [methylated_counts, unmethylated]
+    met_counts = MethylationCountMatrix()  # Sample->(contig,bin_start,bin_end)-> [methylated_counts, unmethylated]
 
     # Define which reads we want to count:
     known =  set()
@@ -524,28 +524,8 @@ def count_methylation_binned(args):
                     else:
                         bin_id = (contig, bin_start, bin_end)
 
-                    if not bin_id in met_counts:
-                        met_counts[bin_id] = {}
-                    if not sample in met_counts[bin_id]:
-                        met_counts[bin_id][sample] = [0,0]
-                    met_counts[bin_id][sample][call=='Z']+=1
+                    met_counts[sample, bin_id][call=='Z']+=1
 
-
-    # Normalize?
-    if kwargs.get('output_fmt', None) == 'methylated':
-        for sample, data in met_counts.items():
-            for bin in data:
-                data[bin] = data[bin][1]
-    elif kwargs.get('output_fmt', None) == 'unmethylated':
-        for sample, data in met_counts.items():
-            for bin in data:
-                data[bin] = data[bin][0]
-    elif kwargs.get('output_fmt', None) == 'both':
-        return met_counts
-    else:
-        for sample, data in met_counts.items():
-            for bin in data:
-                data[bin] = data[bin][1] / (data[bin][0]+data[bin][1])
     return met_counts
 
 
--- singlecellmultiomics/bamProcessing/bamToMethylationCalls.py
+++ singlecellmultiomics/bamProcessing/bamToMethylationCalls.py
@@ -19,6 +19,7 @@ import argparse
 from singlecellmultiomics.bamProcessing import get_samples_from_bam
 from colorama import Fore, Style
 from singlecellmultiomics.utils import dataframe_to_wig
+from singlecellmultiomics.methylation import MethylationCountMatrix
 
 
 def prefilter(counts, cell_names, min_measurements, min_variance):
@@ -35,8 +36,15 @@ def panda_and_prefilter(args):
     return prefilter(pd.DataFrame(d), *args)
 
 
-def get_methylation_count_matrix(bam_path, bin_size, bp_per_job,  min_measurements, min_variance, skip_contigs=['MT'],
-                                 known_variants: str=None, maxtime=None, threads=None, output_fmt=None ):
+def get_methylation_count_matrix(bam_path,
+                                 bin_size: int,
+                                 bp_per_job: int,
+                                 min_samples: int = None,
+                                 min_variance: int = None,
+                                 skip_contigs: set = None,
+                                 known_variants: str = None,
+                                 maxtime: int = None,
+                                 threads: int = None):
 
     commands = generate_commands(
         alignments_path=bam_path,
@@ -47,39 +55,24 @@ def get_methylation_count_matrix(bam_path, bin_size, bp_per_job,  min_measuremen
         bins_per_job=int(bp_per_job / bin_size), min_mq=40,
         kwargs={'known': known_variants,
                 'maxtime': maxtime,
-                'output_fmt':output_fmt
                 }, skip_contigs=skip_contigs
     )
 
-    # We need the cell names a priori for efficient memory management
-    cell_names = get_samples_from_bam(bam_path)
 
-    chunks = []
-
+    count_mat = MethylationCountMatrix()
     if threads==1:
         for command in commands:
-            chunks.append( count_methylation_binned(command) )
+            result = count_methylation_binned(command)
+            result.prune(min_samples=min_samples, min_variance=min_variance)
+            count_mat.update( result )
     else:
         with multiprocessing.Pool(threads) as workers:
 
             for result in workers.imap_unordered(count_methylation_binned, commands):
-                chunks.append(result)
+                result.prune(min_samples=min_samples, min_variance=min_variance)
+                count_mat.update(result)
 
-
-    with multiprocessing.Pool(threads) as workers:
-        filtered_chunks = list(workers.imap(panda_and_prefilter, ( (c,(cell_names, min_measurements, min_variance)) for c in chunks)))
-
-    # Record columns:
-    columns = []
-    for chunk in filtered_chunks:
-        columns += list(chunk.columns)
-
-    # Merge using numpy, because pandas eats the memory like a panda
-    counts_matrix = np.concatenate(filtered_chunks, axis=1)
-    del filtered_chunks
-    del chunks
-
-    return pd.DataFrame(counts_matrix, columns=pd.MultiIndex.from_tuples(columns), index=cell_names)
+    return count_mat
 
 
 
@@ -91,36 +84,39 @@ if __name__ == '__main__':
     argparser.add_argument('bamfile', metavar='bamfile', type=str)
     argparser.add_argument('-bin_size', default=500, type=int)
     argparser.add_argument('-bp_per_job', default=500_000, type=int)
-    argparser.add_argument('-known_variants', help='VCF file with known variants', type=str)
+    argparser.add_argument('-known_variants', help='VCF file with known variants, will be not taken into account as methylated/unmethylated', type=str)
     argparser.add_argument('-threads', default=None, type=int, help='Amount of threads to use, None to use the amount of available threads')
-    argparser.add_argument('-min_variance', default=0.1, type=float)
-    argparser.add_argument('-min_measurements', default=10, type=int)
 
-    argparser.add_argument('-bed', type=str, help='Bed file to write methylation calls to')
-    argparser.add_argument('-wig', type=str, help='WIG file to write mean methylation per bin to')
-    argparser.add_argument('-counts', type=str, help='CSV or pickle file to write single cell counts to')
-    argparser.add_argument('-bismark_tabfile', type=str, help='Tabulated file to write to, contains: chr | start | end | unmethylated_counts | methylated_counts | beta_value')
+    fi = argparser.add_argument_group("Filters")
+    fi.add_argument('-min_variance', default=0.1, type=float)
+    fi.add_argument('-min_min_samples', default=10, type=int)
 
-
-    argparser.add_argument('-ref', help='path to reference fasta', type=str, required=True)
+    og = argparser.add_argument_group("Output")
+    og.add_argument('-bed', type=str, help='Bed file to write methylation calls to')
+    og.add_argument('-wig', type=str, help='WIG file to write mean methylation per bin to')
+    og.add_argument('-counts', type=str, help='CSV or pickle file to write single cell methylation counts to')
+    og.add_argument('-bismark_tabfile', type=str, help='Tabulated file to write to, contains: chr | start | end | unmethylated_counts | methylated_counts | beta_value')
+    og.add_argument('-tabfile', type=str,
+                    help='Tabulated file to write to, contains: chr | start | end | unmethylated_counts | methylated_counts | beta_value | variance | n_samples')
+    og.add_argument('-ref', help='path to reference fasta', type=str, required=True)
+    og.add_argument('-counts', type=str, help='CSV or pickle file to write single cell methylation counts to')
+    og.add_argument('-distmat', type=str, help='CSV or pickle file to write single cell distance matrix to')
     args = argparser.parse_args()
-
 
     counts = get_methylation_count_matrix(bam_path = args.bamfile,
                                  bin_size = args.bin_size,
                                  bp_per_job = args.bp_per_job,
-                                 min_measurements = args.min_measurements,
+                                 min_samples = args.min_samples,
                                  min_variance = args.min_variance,
                                  known_variants = args.known_variants,
-                                 threads=args.threads
-                                          )
+                                 threads=args.threads,
+    )
 
     if args.counts is not None:
         if args.counts.endswith('.csv'):
             counts.to_csv(args.counts)
         else:
             counts.to_pickle(args.counts)
-
 
     if args.wig is not None:
         # Calculate mean beta value and write to wig:
@@ -129,5 +125,16 @@ if __name__ == '__main__':
     if args.bed is not None:
         pass
 
+    if args.distmat is not None:
+
+
+        get_dmat(matrix)
+
     if args.bismark_tabfile is not None:
-        pass
+        counts.get_bulk_frame().to_csv(args.bismark_tabfile, sep='\t')
+
+    if args.tabfile is not None:
+        if args.tabfile.endswith('.csv'):
+            counts.get_bulk_frame().to_csv(args.tabfile, sep='\t')
+        else:
+            counts.get_bulk_frame().to_pickle(args.tabfile)
