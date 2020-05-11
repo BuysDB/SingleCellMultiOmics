a967dc61a23041f2977422d3137275994993997e
BuysDB <git@buysdb.nl>
2020-05-11 22:04:46 +0200

    Wig and distance matrix writing

--- singlecellmultiomics/bamProcessing/bamToMethylationCalls.py
+++ singlecellmultiomics/bamProcessing/bamToMethylationCalls.py
@@ -6,20 +6,15 @@ matplotlib.rcParams['figure.dpi'] = 160
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import pandas as pd
-from glob import glob
 import seaborn as sns
-import pysam
-import numpy as np
 import multiprocessing
-from datetime import datetime
-from singlecellmultiomics.utils.plotting import GenomicPlot
-from singlecellmultiomics.bamProcessing.bamBinCounts import count_fragments_binned, generate_commands, gc_correct_cn_frame, obtain_counts, count_methylation_binned
-import os
+from singlecellmultiomics.bamProcessing.bamBinCounts import generate_commands, count_methylation_binned
 import argparse
-from singlecellmultiomics.bamProcessing import get_samples_from_bam
 from colorama import Fore, Style
 from singlecellmultiomics.utils import dataframe_to_wig
 from singlecellmultiomics.methylation import MethylationCountMatrix
+from singlecellmultiomics.bamProcessing.bamFunctions import get_reference_from_pysam_alignmentFile
+from colorama import Fore,Style
 
 
 def prefilter(counts, cell_names, min_measurements, min_variance):
@@ -41,6 +36,7 @@ def get_methylation_count_matrix(bam_path,
                                  bp_per_job: int,
                                  min_samples: int = None,
                                  min_variance: int = None,
+                                 min_mapping_qual: int = None,
                                  skip_contigs: set = None,
                                  known_variants: str = None,
                                  maxtime: int = None,
@@ -52,7 +48,7 @@ def get_methylation_count_matrix(bam_path,
         key_tags=None,
         max_fragment_size=0,
         dedup=True,
-        bins_per_job=int(bp_per_job / bin_size), min_mq=40,
+        bins_per_job=int(bp_per_job / bin_size), min_mq=min_mapping_qual,
         kwargs={'known': known_variants,
                 'maxtime': maxtime,
                 }, skip_contigs=skip_contigs
@@ -82,59 +78,109 @@ if __name__ == '__main__':
         description="""Export and plot copy number profiles
     """)
     argparser.add_argument('bamfile', metavar='bamfile', type=str)
-    argparser.add_argument('-bin_size', default=500, type=int)
-    argparser.add_argument('-bp_per_job', default=500_000, type=int)
-    argparser.add_argument('-known_variants', help='VCF file with known variants, will be not taken into account as methylated/unmethylated', type=str)
+    argparser.add_argument('-bin_size', default=500, type=int, help='bin size, set to 1 for single CpG')
+    argparser.add_argument('-bp_per_job', default=500_000, type=int, help='Amount of basepairs to be processed per thread per chunk')
     argparser.add_argument('-threads', default=None, type=int, help='Amount of threads to use, None to use the amount of available threads')
+    argparser.add_argument('-ref', help='path to reference fasta', type=str, required=False)
 
     fi = argparser.add_argument_group("Filters")
     fi.add_argument('-min_variance', default=0.1, type=float)
-    fi.add_argument('-min_min_samples', default=10, type=int)
+    fi.add_argument('-min_mapping_qual', default=40, type=int)
+    fi.add_argument('-min_samples', default=10, type=int)
+    fi.add_argument('-skip_contigs', type=str, help='Comma separated contigs to skip', default='MT,chrM')
+    fi.add_argument('-known_variants',
+                           help='VCF file with known variants, will be not taken into account as methylated/unmethylated',
+                           type=str)
 
     og = argparser.add_argument_group("Output")
-    og.add_argument('-bed', type=str, help='Bed file to write methylation calls to')
-    og.add_argument('-wig', type=str, help='WIG file to write mean methylation per bin to')
-    og.add_argument('-counts', type=str, help='CSV or pickle file to write single cell methylation counts to')
+    #og.add_argument('-bed', type=str, help='Bed file to write methylation calls to')
+    og.add_argument('-wig_beta', type=str, help='WIG file to write mean methylation per bin to')
+    og.add_argument('-wig_n_samples', type=str, help='WIG file to write amount of samples covering to')
+
+    og.add_argument('-betas', type=str, help='CSV or pickle file to write single cell methylation betas to')
+    #og.add_argument('-mets', type=str, help='CSV or pickle file to write single cell methylation unmethylated frame to')
+    #og.add_argument('-unmets', type=str, help='CSV or pickle file to write single cell methylation frame to')
+
     og.add_argument('-bismark_tabfile', type=str, help='Tabulated file to write to, contains: chr | start | end | unmethylated_counts | methylated_counts | beta_value')
     og.add_argument('-tabfile', type=str,
                     help='Tabulated file to write to, contains: chr | start | end | unmethylated_counts | methylated_counts | beta_value | variance | n_samples')
-    og.add_argument('-ref', help='path to reference fasta', type=str, required=True)
-    og.add_argument('-counts', type=str, help='CSV or pickle file to write single cell methylation counts to')
     og.add_argument('-distmat', type=str, help='CSV or pickle file to write single cell distance matrix to')
+    og.add_argument('-distmat_plot', type=str, help='.PNG file to write distance matrix image to')
     args = argparser.parse_args()
 
+    if args.distmat_plot is not None and not args.distmat_plot.endswith('.png'):
+        args.distmat_plot += '.png'
+    # autodetect reference:
+    if args.ref is None:
+        args.ref = get_reference_from_pysam_alignmentFile(args.bamfile)
+    if args.ref is None:
+        raise ValueError('Reference autodetect failed. Supply a reference using -ref')
+
+    print('Obtaining counts ', end="")
     counts = get_methylation_count_matrix(bam_path = args.bamfile,
                                  bin_size = args.bin_size,
                                  bp_per_job = args.bp_per_job,
                                  min_samples = args.min_samples,
                                  min_variance = args.min_variance,
                                  known_variants = args.known_variants,
+                                 skip_contigs = args.skip_contigs.split(','),
+                                 min_mapping_qual=args.min_mapping_qual,
                                  threads=args.threads,
     )
+    print(f" [ {Fore.GREEN}OK{Style.RESET_ALL} ] ")
 
-    if args.counts is not None:
-        if args.counts.endswith('.csv'):
-            counts.to_csv(args.counts)
+    if args.betas is not None:
+        print('Writing counts ', end="")
+        if args.betas.endswith('.pickle.gz'):
+            counts.to_pickle(args.betas)
         else:
-            counts.to_pickle(args.counts)
+            counts.get_frame('beta').to_csv(args.betas)
+        print(f" [ {Fore.GREEN}OK{Style.RESET_ALL} ] ")
 
-    if args.wig is not None:
+    if args.wig_beta is not None:
         # Calculate mean beta value and write to wig:
-        dataframe_to_wig(counts.mean().to_frame(), args.wig, span=args.bin_size)
+        print('Writing WIG beta', end="")
+        dataframe_to_wig(counts.get_bulk_frame()[['beta']], args.wig_beta, span=args.bin_size)
+        print(f" [ {Fore.GREEN}OK{Style.RESET_ALL} ] ")
 
-    if args.bed is not None:
-        pass
+    if args.wig_n_samples is not None:
+        # Calculate mean beta value and write to wig:
+        print('Writing WIG n_samples', end="")
+        dataframe_to_wig(counts.get_bulk_frame()[['n_samples']], args.wig_n_samples, span=args.bin_size)
+        print(f" [ {Fore.GREEN}OK{Style.RESET_ALL} ] ")
 
-    if args.distmat is not None:
+    if args.distmat is not None or args.distmat_plot is not None:
+        print('Calculating distance matrix', end="")
+        dmat = counts.get_sample_distance_matrix()
+        print(f" [ {Fore.GREEN}OK{Style.RESET_ALL} ] ")
+        if args.distmat_plot is not None:
+            print('Writing distmat_image', end="")
+            try:
+                sns.clustermap(dmat)
+                plt.tight_layout()
+                plt.savefig(args.distmat_plot)
+                print(f" [ {Fore.GREEN}OK{Style.RESET_ALL} ] ")
+            except Exception as e:
+                print(e)
+                print(f" [ {Fore.RED}FAIL{Style.RESET_ALL} ] ")
 
-
-        get_dmat(matrix)
+        if args.distmat is not None:
+            print('Writing distance matrix', end="")
+            if args.distmat.endswith('.pickle.gz'):
+                dmat.to_pickle(args.distmat)
+            else:
+                dmat.to_csv(args.distmat, sep='\t')
+            print(f" [ {Fore.GREEN}OK{Style.RESET_ALL} ] ")
 
     if args.bismark_tabfile is not None:
+        print('Writing bismark_tabfile matrix', end="")
         counts.get_bulk_frame().to_csv(args.bismark_tabfile, sep='\t')
+        print(f" [ {Fore.GREEN}OK{Style.RESET_ALL} ] ")
 
     if args.tabfile is not None:
-        if args.tabfile.endswith('.csv'):
-            counts.get_bulk_frame().to_csv(args.tabfile, sep='\t')
-        else:
+        print('Writing tabfile', end="")
+        if args.tabfile.endswith('.pickle.gz'):
             counts.get_bulk_frame().to_pickle(args.tabfile)
+        else:
+            counts.get_bulk_frame().to_csv(args.tabfile, sep='\t')
+        print(f" [ {Fore.GREEN}OK{Style.RESET_ALL} ] ")
