6380eb1e217aa26b503b4c2a409ba56be33a4092
BuysDB <git@buysdb.nl>
2020-10-01 13:49:13 +0200

    Added scartrace class which includes a random sequence, and added autodetection

--- singlecellmultiomics/modularDemultiplexer/demultiplexModules/scartrace.py
+++ singlecellmultiomics/modularDemultiplexer/demultiplexModules/scartrace.py
@@ -1,4 +1,4 @@
-from singlecellmultiomics.modularDemultiplexer.baseDemultiplexMethods import UmiBarcodeDemuxMethod
+from singlecellmultiomics.modularDemultiplexer.baseDemultiplexMethods import UmiBarcodeDemuxMethod, NonMultiplexable
 
 # ScarTrace
 
@@ -23,6 +23,8 @@ class ScartraceR1(UmiBarcodeDemuxMethod):
         self.autoDetectable = True
 
 
+
+
 class ScartraceR2(UmiBarcodeDemuxMethod):
     def __init__(self, barcodeFileParser, **kwargs):
         self.barcodeFileAlias = 'scartrace'
@@ -41,3 +43,49 @@ class ScartraceR2(UmiBarcodeDemuxMethod):
         self.longName = 'Scartrace, CB: 8bp'
         self.description = '384 well format. Scar amplicon demultiplexing, cell barcode in read 2'
         self.autoDetectable = True
+
+    def demultiplex(self, records, **kwargs):
+
+        if kwargs.get(
+                'probe') and not records[0].sequence.startswith('CCTTGAACTTCTGGTTGTAG'):
+            raise NonMultiplexable
+
+        taggedRecords = UmiBarcodeDemuxMethod.demultiplex(
+                self, records, **kwargs)
+        return taggedRecords
+
+
+
+class ScartraceR2RP4(UmiBarcodeDemuxMethod):
+    def __init__(self, barcodeFileParser, **kwargs):
+        self.barcodeFileAlias = 'scartrace'
+        UmiBarcodeDemuxMethod.__init__(
+            self,
+            umiRead=0,
+            umiStart=0,
+            umiLength=0,
+            barcodeRead=1,
+            barcodeStart=0,
+            barcodeLength=8,
+            barcodeFileAlias=self.barcodeFileAlias,
+            barcodeFileParser=barcodeFileParser,
+
+            random_primer_end=False,
+            random_primer_read=0,
+            random_primer_length=4,
+
+            **kwargs)
+        self.shortName = 'SCARC8R2R4'
+        self.longName = 'Scartrace, CB: 8bp, with 4bp random sequence in read 1'
+        self.description = '384 well format. Scar amplicon demultiplexing, cell barcode in read , 4bp random sequence in R1'
+        self.autoDetectable = True
+
+    def demultiplex(self, records, **kwargs):
+
+        if kwargs.get(
+                'probe') and not records[0].sequence[4:].startswith('CCTTGAACTTCTGGTTGTAG'):
+            raise NonMultiplexable
+
+        taggedRecords = UmiBarcodeDemuxMethod.demultiplex(
+                self, records, **kwargs)
+        return taggedRecords
