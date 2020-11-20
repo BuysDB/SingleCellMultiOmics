8da3019ef2969fefb98d2c4d058b7614829480c0
BuysDB <git@buysdb.nl>
2020-11-20 11:53:40 +0100

    Added desc.

--- singlecellmultiomics/utils/poolutils.py
+++ singlecellmultiomics/utils/poolutils.py
@@ -1,3 +1,12 @@
 def pool_wrapper(args):
+    """
+    Wraps a function, so it can be natively called from a Pool
+
+    Args:
+        f, kwargs : f is the function to call, kwargs a dictionary of arguments to pass to f
+
+    Returns:
+        return value of f called with **kwargs
+    """
     f, kwargs =  args
     return f(**kwargs)
