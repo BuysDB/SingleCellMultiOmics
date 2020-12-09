af415dd6d9ba31e1786dfee78b3a5c36d8c5992d
BuysDB <git@buysdb.nl>
2020-12-09 18:00:31 +0100

    Added bigwig writer

--- singlecellmultiomics/utils/export.py
+++ singlecellmultiomics/utils/export.py
@@ -1,4 +1,9 @@
 import pandas as pd
+from singlecellmultiomics.bamProcessing import get_contig_sizes
+from collections import defaultdict
+import pysam
+import pyBigWig
+import numpy as np
 
 def dataframe_to_wig(df: pd.DataFrame, wig_path: str, span: int = 1, stepper: str = "variableStep",
                      graphType: str = "points", offset: int = 1, extra_header=''):
@@ -25,3 +30,45 @@ def dataframe_to_wig(df: pd.DataFrame, wig_path: str, span: int = 1, stepper: st
                 else:
                     raise ValueError('Supply a dataframe with a multi index in the format (chrom, pos) ')
                 out.write(f"{pos+offset}\t{row.iloc[0] if 'value' not in row else row['value']}\n")
+
+
+def write_bigwig(write_locations:dict, write_values:dict,  source_bam:str, write_path:str,bin_size: int=None):
+    """
+    Write location / values to a bigwig file
+
+    Args:
+        write_locations(dict) : Locations to write to, grouped by contig, {contig:[location (int), ..]
+
+        write_values(dict) : Value to write, grouped by contig {contig:[value (float), ..],
+
+        source_bam(str): path to source bam file to extract contig ordering from
+
+        write_path(str): write bigwig file here
+
+        bin_size(int) : bin_size, set to None to use a bin size of 1
+
+    """
+    with pysam.AlignmentFile(source_bam) as alignments, pyBigWig.open(write_path,'w') as out:
+
+        cs = get_contig_sizes(alignments)
+        # Write header
+        out.addHeader(list(zip(alignments.references, alignments.lengths)))
+
+        for contig in alignments.references:
+            if not contig in write_locations:
+                continue
+
+            contig_list = [contig]*len(write_locations[contig])
+            start = sorted(list(write_locations[contig]))
+            if bin_size is None:
+                end = np.array(start)+1
+            else:
+                end = np.array(start)+bin_size
+
+            values_to_write = np.array( [write_values[contig][p] for p in start] , dtype=np.float32)
+
+            out.addEntries(
+                contig_list, #Contig
+                start, #Start
+                ends= end, #end
+                values= values_to_write)
