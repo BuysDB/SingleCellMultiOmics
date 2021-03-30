4fdd229370f4b423ca3684364427d9063e6c8cad
BuysDB <git@buysdb.nl>
2021-03-30 22:47:12 +0200

    Added usage warning

--- singlecellmultiomics/fragment/nlaIII.py
+++ singlecellmultiomics/fragment/nlaIII.py
@@ -29,7 +29,9 @@ class NlaIIIFragment(Fragment):
 
         if self.no_overhang and reference  is None:
             raise ValueError('Supply a reference handle when no_overhang=True')
-
+        if self.no_overhang and not self.check_motif:
+            raise ValueError('no_overhang=True is not compatible with check_motif=False, as there is no way to deduplicate. Consider using method "chic"')
+            
         Fragment.__init__(self,
                           reads,
                           assignment_radius=assignment_radius,
@@ -169,7 +171,8 @@ class NlaIIIFragment(Fragment):
             self.set_rejection_reason('unmapped R1')
             return None
 
-        if self.no_overhang:
+        if self.no_overhang
+
             # scan 3 bp of sequence for CATG
             scan_extra_bp = 3
             site_coordinate = None
@@ -230,9 +233,6 @@ class NlaIIIFragment(Fragment):
             return(rpos)
 
         else:
-            assert( self.check_motif )# should never happen. It means R1 is not forward nor reverse
-
-
 
             if forward_motif == 'CATG' and R1.is_reverse:
                 self.set_rejection_reason('found CATG R1 REV exp FWD', set_qcfail=True)
