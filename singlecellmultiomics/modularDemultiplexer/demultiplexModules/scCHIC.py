3b7baedb30933bbb965ba4fa53708bdc6554089a
BuysDB <git@buysdb.nl>
2021-06-30 14:48:55 +0200

    Added single end chic support

--- singlecellmultiomics/modularDemultiplexer/demultiplexModules/scCHIC.py
+++ singlecellmultiomics/modularDemultiplexer/demultiplexModules/scCHIC.py
@@ -135,7 +135,61 @@ class SCCHIC_384w_c8_u3_direct_ligation(UmiBarcodeDemuxMethod):
         return taggedRecords
 
 
+class SCCHIC_384w_c8_u3_direct_ligation_SINGLE_END(UmiBarcodeDemuxMethod):
+    def __init__(self, barcodeFileParser, random_primer_read=None,random_primer_length=None, **kwargs):
+        self.barcodeFileAlias = 'maya_384NLA'
+        UmiBarcodeDemuxMethod.__init__(
+            self,
+            umiRead=0,
+            umiStart=0,
+            umiLength=3,
+            barcodeRead=0,
+            barcodeStart=3,
+            barcodeLength=8,
+            random_primer_read=None,
+            random_primer_length=None,
+            barcodeFileAlias=self.barcodeFileAlias,
+            barcodeFileParser=barcodeFileParser,
+            **kwargs)
+        self.shortName = 'scCHIC384C8U3se'
+        self.longName = 'Single cell CHIC, 384well CB: 8bp UMI: 3bp, single end, no RP'
+        self.autoDetectable = True
+        self.description = '384 well format. 3bp umi followed by 8bp barcode and a single A. No read 2'
 
+        self.sequenceCapture[0] = slice(
+            self.barcodeLength + self.umiLength + 1,
+            None)  # dont capture the first base
+
+    def demultiplex(self, records, **kwargs):
+
+        if kwargs.get(
+                'probe') and records[0].sequence[self.barcodeLength + self.umiLength] != 'T':
+            raise NonMultiplexable
+
+        if len(records) != 1:
+            raise NonMultiplexable
+
+        # add first 2 bases as ligation tag:
+        ligation_start = self.barcodeLength + self.umiLength
+        ligation_end = ligation_start + 2
+        ligation_sequence = records[0].sequence[ligation_start:ligation_end]
+        ligation_qualities = records[0].qual[ligation_start:ligation_end]
+
+        taggedRecords = UmiBarcodeDemuxMethod.demultiplex(
+            self, records, **kwargs)
+
+        taggedRecords[0].addTagByTag(
+            'lh',
+            ligation_sequence,
+            isPhred=False,
+            make_safe=False)
+        taggedRecords[0].addTagByTag(
+            'lq',
+            ligation_qualities,
+            isPhred=True,
+            make_safe=False)
+
+        return taggedRecords
 
 class SCCHIC_384w_c8_u3_pdt(IlluminaBaseDemultiplexer):
 
