b164da53c3ddb462ccfbadbb15fb8ab29837b5dc
git@buysdb.nl <git@buysdb.nl>
2022-03-09 21:10:16 +0100

    Write read groups correctly

--- singlecellmultiomics/bamProcessing/bamExtractSamples.py
+++ singlecellmultiomics/bamProcessing/bamExtractSamples.py
@@ -8,7 +8,7 @@ from singlecellmultiomics.bamProcessing import get_sample_to_read_group_dict
 import collections
 from singlecellmultiomics.utils.path import get_valid_filename
 
-def extract_samples( input_bam_path, output_path, capture_samples, head=None ):
+def extract_samples( input_bam_path, output_path, capture_samples, head=None, write_group_rg=False, rg_group_prefix=None ):
     """
     Extract samples from a bam file
 
@@ -23,29 +23,56 @@ def extract_samples( input_bam_path, output_path, capture_samples, head=None ):
 
     """
     bamFile = pysam.AlignmentFile(input_bam_path, "rb")
-    header = bamFile.header.copy()
 
     # Write to multiple files:
     output_handles = {}
     print('Groups:')
     for group in capture_samples:
 
+        header = bamFile.header.copy()
+
+        if write_group_rg:
+
+            if rg_group_prefix:
+                rg = rg_group_prefix + group
+            else:
+                rg = group
+            header = header.to_dict()
+            header['RG'] = [{'ID': rg,
+            'LB': rg,
+            'PL': 'ILLUMINA',
+            'SM': rg,
+            'PU': '1'}] # reset read group header
+
         output_handles[group] = pysam.AlignmentFile(output_path.replace('.bam',f'{group}.bam'), "wb", header=header)
         print(f'\t{group}\t{output_handles[group].filename.decode()}')
 
     sample2handle = {}
+    sample2group = {}
     for group,samples in capture_samples.items():
         for sample in samples:
+            if sample in sample2handle:
+                raise ValueError(f'The sample {sample} is assigned to more than one group at least:{group} and {sample2group[sample]}')
+            print(f'\t{sample}\t->{group}')
             sample2handle[sample] = output_handles[group]
+            sample2group[sample] = group
 
     written = 0
     for r in bamFile:
-
+        sm = r.get_tag('SM')
         try:
-            sample2handle[r.get_tag('SM')].write(r)
+            handle = sample2handle[sm]
         except KeyError:
             continue
 
+        if write_group_rg:
+            if rg_group_prefix:
+                r.set_tag('RG', rg_group_prefix + sample2group[sm] )
+            else:
+                r.set_tag('RG', sample2group[sm] )
+
+
+        handle.write(r)
         written += 1
         if head is not None and written >= head:
             break
@@ -77,6 +104,8 @@ if __name__ == '__main__':
         action='store_true',
         help='Force overwrite of existing files')
     argparser.add_argument('-head', type=int)
+    argparser.add_argument('--write_group_rg', action='store_true', help='Set the group label as read-group, requires second GROUP column in the input file')
+    argparser.add_argument('-rg_group_prefix', type=str, help='Prepend this string tot the read group. Requires --write_group_rg ')
     args = argparser.parse_args()
 
     if os.path.exists(args.o) and not args.f:
@@ -103,4 +132,4 @@ if __name__ == '__main__':
                 raise ValueError("Please supply a file with one or two columns: [SAMPLE], or [SAMPLE]{tab}[GROUP]")
             capture_samples[group].add( parts[0] )
 
-    extract_samples(args.bamfile, args.o, capture_samples, head=args.head )
+    extract_samples(args.bamfile, args.o, capture_samples, head=args.head, write_group_rg=args.write_group_rg, rg_group_prefix=args.rg_group_prefix )
