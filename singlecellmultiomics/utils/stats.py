c6437c43310098b55c1426ccde8b13d9cc149094
git@buysdb.nl <git@buysdb.nl>
2022-03-17 12:26:16 +0100

    Added bw to density functions

--- singlecellmultiomics/utils/stats.py
+++ singlecellmultiomics/utils/stats.py
@@ -7,6 +7,8 @@ import statsmodels.formula.api as smf
 from singlecellmultiomics.utils import pool_wrapper
 from multiprocessing import Pool
 from scipy.ndimage import gaussian_filter
+import pyBigWig
+from typing import Callable, Union
 
 def calculate_nested_f_statistic(small_model, big_model):
     # From https://stackoverflow.com/a/60769343/2858160
@@ -32,6 +34,62 @@ def gaussian_2d(df:pd.DataFrame, sigmas:tuple, **kwargs) -> pd.DataFrame:
         index=df.index,
         columns=df.columns)
     return df
+
+
+def vectorize_bw(path: str, bin_size: int, selected_contigs:list ,sample=None) -> pd.Series:
+    """Vectorize (numpy) a bigwig file
+
+    Args:
+        path: path to bigwig file
+        bin_size : bin size
+        selected_contigs: list of contigs to take into account
+        sample: name of output series, tuple for MultiIndex
+     """
+
+    bin_labels=[]
+    densities= []
+    with pyBigWig.open(path) as handle:
+        for contig, size in handle.chroms().items():
+            if not contig in selected_contigs:
+                continue
+            nbins = int(size/bin_size)+1
+            density = np.zeros( nbins)
+            for bin_index, bin_start in enumerate(range(0,size,bin_size)):
+                bin_end = bin_start + bin_size
+                bin_end=  min(bin_end,size-1)
+                try:
+                    density[bin_index] = np.array( handle.intervals(contig,bin_start, bin_end) )[:,2].mean()
+                except IndexError:
+                    pass
+
+                bin_labels.append((contig,bin_start,bin_end))
+            densities.append(density)
+    # Convert to series:
+    return  pd.Series(np.concatenate(densities),index=bin_labels,name=sample)
+
+def vectorize_bw_multi(paths: list, selected_contigs:list, formatsample: Callable[ [str,], Union[tuple,str,int] ], bin_size:int, n_threads=None):
+
+    commands = [
+        (
+            vectorize_bw,
+            {
+                'sample' : formatsample(path),
+                'path' : path,
+                'bin_size': bin_size,
+                'selected_contigs':selected_contigs
+            }
+        )
+        for path in paths
+    ]
+    with Pool(n_threads) as workers:
+        densities=[]
+        for ser in workers.imap(
+            pool_wrapper,
+            commands
+        ):
+            densities.append(ser)
+
+    return pd.concat(densities,axis=1).T
 
 
 def _GLM_cluster_de_test_single_gene(gene, cuts_frame, clusters):
